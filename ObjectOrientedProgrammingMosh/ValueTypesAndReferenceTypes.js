e856cf89d635c3445aa6c217f6901417eb7fac88
Scheibelr <scheibelr@gmail.com>
2022-07-29 14:27:56 -0400

    abstraction

---
+++ ObjectOrientedProgrammingMosh/ValueTypesAndReferenceTypes.js
@@ -0,0 +1,7 @@
+//Value types aka 'primitives' include [Number, String, Boolean, Symbol, undefined, null]
+//Reference types include [Object, Function, Array]
+
+let x = { value: 10 };
+let y = x;
+
+x.value = 20;
--- ObjectOrientedProgrammingMosh/factoriesAndConstructors.js
+++ ObjectOrientedProgrammingMosh/factoriesAndConstructors.js
@@ -1,8 +1,10 @@
 //if an object has 1 or more method, we say it has 'Behavior'
 
-function createCircle() {
+//the following is a 'Factory Function'
+
+function createCircle(radius) {
   return {
-    //if the key and the value are the same, we can just write the key.
+    //if the key and the value are the same, we can just write the key. ('radius: radius,' == 'radius,' )
     radius,
     draw: function () {
       console.log("draw");
@@ -12,3 +14,17 @@ function createCircle() {
 
 const circle = createCircle(1);
 circle.draw();
+
+// Constructor Function
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+const another = new Circle(1);
+
+//using '.this' keyword with a new operator is a constructor. Using a function that returns an object is called a factory function. I prefer factory functions.
+
+//in javascript, functions are objects.
